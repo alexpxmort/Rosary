2f7d115ef220a1dafb3ff86e8f7cc369f0b602ea
Alex <lexpdigi@gmail.com>
2022-12-06 11:36:05 -0300

    feat: add speech

--- src/pages/terco/terco.component.ts
+++ src/pages/terco/terco.component.ts
@@ -2,16 +2,57 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { IPrayer, TercoService } from '../../app/terco.service';
 
-var Speech = require('speak-tts')
+const checkBrowserCompatibility = () => {
+  return "speechSynthesis" in window
 
+}
+
+
+interface RecommendedVoices {
+	[key: string]: boolean;
+}
 
 @Component({
   templateUrl: './terco.component.html',
   styleUrls: ['./terco.component.scss']
 })
 export class TercoComponent implements OnInit {
+  public recommendedVoices: RecommendedVoices;
 
-  constructor(private tercoService:TercoService,private render2:Renderer2){}
+  constructor(private tercoService:TercoService,private render2:Renderer2){
+    this.voices = [];
+		this.rates = [ .25, .5, .75, 1, 1.25, 1.5, 1.75, 2 ];
+		this.selectedVoice = null;
+		this.selectedRate = 1;
+		// Dirty Dancing for the win!
+		this.text = "Me? ... I'm scared of everything. I'm scared of what I saw, of what I did, of who I am. And most of all, I'm scared of walking out of this room and never feeling the rest of my whole life ... the way I feel when I'm with you.";
+
+		// These are "recommended" in so much as that these are the voices that I (Ben)
+		// could understand most clearly.
+		this.recommendedVoices = Object.create( null );
+		this.recommendedVoices[ "Alex" ] = true;
+		this.recommendedVoices[ "Alva" ] = true;
+		this.recommendedVoices[ "Damayanti" ] = true;
+		this.recommendedVoices[ "Daniel" ] = true;
+		this.recommendedVoices[ "Fiona" ] = true;
+		this.recommendedVoices[ "Fred" ] = true;
+		this.recommendedVoices[ "Karen" ] = true;
+		this.recommendedVoices[ "Mei-Jia" ] = true;
+		this.recommendedVoices[ "Melina" ] = true;
+		this.recommendedVoices[ "Moira" ] = true;
+		this.recommendedVoices[ "Rishi" ] = true;
+		this.recommendedVoices[ "Samantha" ] = true;
+		this.recommendedVoices[ "Tessa" ] = true;
+		this.recommendedVoices[ "Veena" ] = true;
+		this.recommendedVoices[ "Victoria" ] = true;
+		this.recommendedVoices[ "Yuri" ] = true;
+  }
+
+  public rates!: number[];
+	public selectedRate!: number;
+	public selectedVoice!: SpeechSynthesisVoice | null;
+	public text: string = '';
+	public voices!: SpeechSynthesisVoice[];
 
   prayer$:Observable<any> =  this.tercoService.getPrayer()
 
@@ -21,30 +62,109 @@ export class TercoComponent implements OnInit {
 
   speech!:any;
 
+	public demoSelectedVoice() : void {
+
+		if ( ! this.selectedVoice ) {
+
+			console.warn( "Expected a voice, but none was selected." );
+			return;
+
+		}
+
+		var demoText = "Best wishes and warmest regards.";
+
+		this.stop();
+		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, demoText );
+
+	}
+
+	public speak() : void {
+
+		if ( ! this.selectedVoice || ! this.text ) {
+
+      console.log(`aqui`)
+			return;
+
+		}
+
+		this.stop();
+		this.synthesizeSpeechFromText( this.selectedVoice, this.selectedRate, this.text );
+
+	}
+
+  // I stop any current speech synthesis.
+	public stop() : void {
+
+		if ( speechSynthesis.speaking ) {
+
+			speechSynthesis.cancel();
+
+		}
+
+	}
+
+// I perform the low-level speech synthesis for the given voice, rate, and text.
+private synthesizeSpeechFromText(
+  voice: SpeechSynthesisVoice,
+  rate: number,
+  text: string
+  ) : void {
+
+  var utterance = new SpeechSynthesisUtterance( text );
+  utterance.voice = this.selectedVoice;
+  utterance.rate = rate;
+
+  speechSynthesis.speak( utterance );
+
+  utterance.onend = () =>console.log(`TEXTO JA FOI FALADO ${text}`)
+
+}
+
+	// I update the "say" command that can be used to generate the a sound file from the
+	// current speech synthesis configuration.
+	public updateSayCommand() : void {
+
+		if ( ! this.selectedVoice || ! this.text ) {
+
+			return;
+
+		}
+
+		// With the say command, the rate is the number of words-per-minute. As such, we
+		// have to finagle the SpeechSynthesis rate into something roughly equivalent for
+		// the terminal-based invocation.
+		var sanitizedRate = Math.floor( 200 * this.selectedRate );
+		var sanitizedText = this.text
+			.replace( /[\r\n]/g, " " )
+			.replace( /(["'\\\\/])/g, "\\$1" )
+		;
+
+    this.text = sanitizedText;
+
+
+	}
+
+
   ngOnInit(): void {
-     this.speech = new Speech.default() // will throw an exception if not browser supported
+    this.voices = speechSynthesis.getVoices();
+		this.selectedVoice = ( this.voices[ 0 ] || null );
+		this.updateSayCommand();
 
+		// The voices aren't immediately available (or so it seems). As such, if no
+		// voices came back, let's assume they haven't loaded yet and we need to wait for
+		// the "voiceschanged" event to fire before we can access them.
+		if ( ! this.voices.length ) {
 
-    if (this.speech.hasBrowserSupport()) {
-      this.speech.init({
-        'volume': 1,
-           'lang': 'pt-BR',
-           'rate': 1,
-           'pitch': 1,
-           'voice':"Google português do Brasil",
-           'splitSentences': true,
-           'listeners': {
-               'onvoiceschanged': (voices:any) => {
-                   console.log("Event voiceschanged", voices)
-               }
-           }
-        }).then((data:any) => {
+			speechSynthesis.addEventListener(
+				"voiceschanged",
+				() => {
 
-          // The "data" object contains the list of available voices and the voice synthesis params
-          console.log("Speech is ready, voices are available", data)
-      }).catch((e:any) => {
-          console.error("An error occured while initializing : ", e)
-      });
+					this.voices = speechSynthesis.getVoices();
+					this.selectedVoice = ( this.voices[ 0 ] || null );
+					this.updateSayCommand();
+
+				}
+			);
 
     }
 
@@ -56,6 +176,8 @@ export class TercoComponent implements OnInit {
 
 
   async handlePrayer(name:string,event:any){
+
+
 
     const MAX_PRAYERS:number = 15;
     const COLOR_SELECTED_PRAYER = 'darkblue';
@@ -84,15 +206,18 @@ export class TercoComponent implements OnInit {
     const content  = document.querySelector(`.content p`)?.textContent
 
 
-    try{
+    this.text = content || '';
+    if(checkBrowserCompatibility()){
 
-    await  this.speech.speak({text:content})
 
 
-    } catch(err){
-      console.log(err)
+      this.speak();
+
+
+
+
+
     }
-
 
     const selectedPrayersCount = Array.from(document.querySelectorAll('div')).filter((el) => el.getAttribute(`style`)?.includes(COLOR_SELECTED_PRAYER))
 
