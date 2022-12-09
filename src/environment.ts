feea4c7368f14b382646d4857ae663afcbe89250
Alex <lexpdigi@gmail.com>
2022-12-09 09:24:20 -0300

    feat: homilia today

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  LINK_HOMILIA: 'https://www.youtube.com/embed/rXwWef7U5OY',
+  LINK_HOMILIA: 'https://www.youtube.com/embed/pzRibtorPCo',
   LINKS_SAO_JOSE:[
     'https://www.youtube.com/embed/MvlCL_v0L1I',
     'https://www.youtube.com/embed/qR5YLl-ojdI',
