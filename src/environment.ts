b487fc6a02ba616bda1678aee383f1994c39f216
Alex <lexpdigi@gmail.com>
2022-12-23 08:02:07 -0300

    feat: add links

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: true,
-  LINK_HOMILIA: 'https://www.youtube.com/embed/Ur5V2P5Js14',
-  LINK_3_MESES_SAO_JOSE: 'https://www.youtube.com/embed/HL9UqA3rY3w',
+  LINK_HOMILIA: 'https://www.youtube.com/embed/abTxyCaFXtM',
+  LINK_3_MESES_SAO_JOSE: 'https://www.youtube.com/embed/t8VJLDV_OEM',
   LINK_COROINHA: 'https://www.youtube.com/embed/GwZ7i0ld36U',
   LINKS_SAO_JOSE:[
     'https://www.youtube.com/embed/MvlCL_v0L1I',
