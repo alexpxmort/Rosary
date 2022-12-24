8e182db4009d759f972b8191d4cab659a60a2d37
Alex <lexpdigi@gmail.com>
2022-12-24 09:37:43 -0300

    feaT: update links

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   LINK_HOMILIA: 'https://www.youtube.com/embed/6kR9jSaC77A',
-  LINK_3_MESES_SAO_JOSE: 'https://www.youtube.com/embed/K4T3MJ8',
+  LINK_3_MESES_SAO_JOSE: 'https://www.youtube.com/embed/Byw-K4T3MJ8',
   LINK_COROINHA: 'https://www.youtube.com/embed/GwZ7i0ld36U',
   LINKS_SAO_JOSE:[
     'https://www.youtube.com/embed/MvlCL_v0L1I',
