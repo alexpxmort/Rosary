2d80fa1be215cfa4e7ed984fb1594dee0625b490
Alex <lexpdigi@gmail.com>
2023-01-01 09:13:10 -0300

    homilia 2023

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: true,
-  LINK_HOMILIA: 'https://www.youtube.com/embed/ctVCm0rLMk',
+  LINK_HOMILIA: 'https://www.youtube.com/embed/-ctVCm0rLMk',
   LINK_3_MESES_SAO_JOSE: 'https://www.youtube.com/embed/Qairi87hm28',
   LINK_COROINHA: 'https://www.youtube.com/embed/GwZ7i0ld36U',
   LINKS_SAO_JOSE:[
